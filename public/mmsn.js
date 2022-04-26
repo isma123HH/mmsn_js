6d742a8ebbd7abb74c08ae15e14d7b6c83e3bced
isma <102177279+isma123HH@users.noreply.github.com>
2022-04-26 18:53:56 +0800

    Update mmsn.js

--- public/mmsn.js
+++ public/mmsn.js
@@ -1,8 +1,10 @@
 import swal2  from 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
 
-const NAMESPACE = 'test'
 
-function mmsn_log(NAMESPACE,description,msg){
-    var ok_log = console.log(`${NAMESPACE}: ${description}`, msg)
-    return ok_log
+function mmsn(){
+    const NAMESPACE = 'test'
+    this.log = function(description,msg){
+        var ok_log = console.log(`${NAMESPACE}: ${description}`, msg)
+        return ok_log
+    }
 }
