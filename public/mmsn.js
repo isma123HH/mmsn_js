65b83579965f3d957535676ad9cae43d1211bdcf
isma <102177279+isma123HH@users.noreply.github.com>
2022-04-26 18:43:30 +0800

    Add files via upload

---
+++ public/mmsn.js
@@ -0,0 +1,7 @@
+import swal2  from 'https://cdn.jsdelivr.net/npm/sweetalert2@11';
+
+const NAMESPACE = 'test'
+
+function mmsn_log(NAMESPACE,description,msg){
+    console.log(`${NAMESPACE}: ${description}`, msg)
+}
